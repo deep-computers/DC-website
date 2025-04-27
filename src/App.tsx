19bd8fab69207b4a6763d761b330c87d0f8c2cdf
therajes <your-therajes-github-email@example.com>
2025-04-27 18:59:23 +0530

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { ThemeProvider } from "./contexts/ThemeProvider";
+import ScrollToTop from "./components/ui/ScrollToTop";
 import Index from "./pages/Index";
 import PrintOrder from "./pages/PrintOrder";
 import BindingOrder from "./pages/BindingOrder";
@@ -23,6 +24,7 @@ const App = () => (
         <Toaster />
         <Sonner />
         <BrowserRouter>
+          <ScrollToTop />
           <Routes>
             <Route path="/" element={<Index />} />
             <Route path="/print-order" element={<PrintOrder />} />
